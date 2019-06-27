b6cd6172b1a5e245daa7ee579fbca8e17b6670b5
JasonWang <jstzwj@aliyun.com>
2019-06-28 00:37:14 +0800

    remove vscode

--- build.py
+++ build.py
@@ -9,34 +9,54 @@ def main(argv):
     machine_type = platform.machine()
     is_debug = False
 
-    traph_build = argv[0]
-    traph_root, build_file = os.path.split(traph_build)
+    traph_build_script = os.path.abspath(sys.argv[0])
+    traph_root, build_file = os.path.split(traph_build_script)
     if not os.path.exists(os.path.join(traph_root, "build")):
         os.mkdir("build")
     os.chdir(traph_root + "/build")
-    
-    if machine_type == 'AMD64':
+
+    if system_type == 'Windows':
+        if machine_type == 'x86_64':
+            # -DCMAKE_BUILD_TYPE=Debug
+            if is_debug:
+                subprocess.run("cmake -DCMAKE_BUILD_TYPE=Debug -G \"Visual Studio 15 2017 Win64\" ../")
+                subprocess.run("cmake --build . --config Debug")
+            else:
+                subprocess.run("cmake -DCMAKE_BUILD_TYPE=Release -G \"Visual Studio 15 2017 Win64\" ../")
+                subprocess.run("cmake --build . --config Release")
+            
+        elif machine_type == 'x86':
+            if is_debug:
+                subprocess.run("cmake -DCMAKE_BUILD_TYPE=Debug -G \"Visual Studio 15 2017\" ../")
+                subprocess.run("cmake --build . --config Debug")
+            else:
+                subprocess.run("cmake -DCMAKE_BUILD_TYPE=Release -G \"Visual Studio 15 2017\" ../")
+                subprocess.run("cmake --build . --config Release")
+        else:
+            print('unsupport machine')
+    elif system_type == 'Linux':
         # -DCMAKE_BUILD_TYPE=Debug
         if is_debug:
-            subprocess.run("cmake -DCMAKE_BUILD_TYPE=Debug -G \"Visual Studio 15 2017 Win64\" ../")
+            subprocess.run("cmake -DCMAKE_BUILD_TYPE=Debug -G \"Unix Makefiles\" ../")
             subprocess.run("cmake --build . --config Debug")
         else:
-            subprocess.run("cmake -DCMAKE_BUILD_TYPE=Release -G \"Visual Studio 15 2017 Win64\" ../")
-            subprocess.run("cmake --build . --config Release")
-        
-    elif machine_type == 'x86':
-        if is_debug:
-            subprocess.run("cmake -DCMAKE_BUILD_TYPE=Debug -G \"Visual Studio 15 2017\" ../")
-            subprocess.run("cmake --build . --config Debug")
-        else:
-            subprocess.run("cmake -DCMAKE_BUILD_TYPE=Release -G \"Visual Studio 15 2017\" ../")
+            subprocess.run("cmake -DCMAKE_BUILD_TYPE=Release -G \"Unix Makefiles\" ../")
             subprocess.run("cmake --build . --config Release")
     else:
-        print('unsupport machine')
+        print('unsupport system')
+    
+    
 
     os.chdir("../")
     
     if system_type == 'Windows':
+        if is_debug:
+            shutil.copy('build/traph/source/interface/traph_tensor.py', 'python/pytraph/core')
+            shutil.copy('build/traph/source/interface/Debug/_traph_tensor.pyd', 'python/pytraph/core')
+        else:
+            shutil.copy('build/traph/source/interface/traph_tensor.py', 'python/pytraph/core')
+            shutil.copy('build/traph/source/interface/Release/_traph_tensor.pyd', 'python/pytraph/core')
+    elif system_type == 'Linux':
         if is_debug:
             shutil.copy('build/traph/source/interface/traph_tensor.py', 'python/pytraph/core')
             shutil.copy('build/traph/source/interface/Debug/_traph_tensor.pyd', 'python/pytraph/core')
